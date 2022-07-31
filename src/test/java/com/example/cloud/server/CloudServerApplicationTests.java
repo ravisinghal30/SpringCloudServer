26a44db23eecfd5a0599f5977b05653bc8d1b72d
Ravi Singhal <ravi_singhal30>
2022-07-31 15:18:50 +0530

    Added new project
    Added new File

---
+++ src/test/java/com/example/cloud/server/CloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
